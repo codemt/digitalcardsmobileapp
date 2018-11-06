dc3c5b1a324f25ed0363f6a38fb8d0a32aaf6258
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-11-06 16:35:20 +0530

    changes made to hoem page

--- src/components/drawer/home/index.js
+++ src/components/drawer/home/index.js
@@ -1,14 +1,74 @@
 import React, { Component } from 'react'
-import { View , Text } from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Container } from 'native-base';
+import { Toolbar } from 'react-native-material-ui';
  class Home extends Component {
   render() {
     return (
-      <View>
-
-            <Text> HEllo Home   </Text>
-        
-      </View>
+        <Container style={styles.container}>
+                        <Toolbar
+                        style={{ container: { backgroundColor: 'black' },centerElementContainer: {alignContent:'center'}}}
+                        leftElement="menu"
+                        centerElement="My Cards"
+                        searchable={{
+                        autoFocus: true,
+                        placeholder: 'Search',
+                        }}
+                        
+                    
+                    
+                    /> 
+        </Container>
     )
   }
 }
 export default Home
+const styles = StyleSheet.create({
+
+
+    form : { 
+
+
+        backgroundColor: 'black',
+        marginTop : 60,
+        
+
+    },
+    container : {
+
+
+
+        backgroundColor : 'grey'
+
+    },
+    title: {
+
+      // resizeMode : 'contain',
+      //  paddingTop : 100,
+        alignSelf: 'center',
+        height:100,
+        //width:400
+
+  },
+  button : {
+
+      marginTop : 15,
+      width:400,
+      alignSelf: 'center',
+      backgroundColor : 'black'
+
+
+  },
+  social : {
+
+      flex:  1 ,
+      flexDirection: 'row',
+      padding: 15,
+      alignSelf: 'center',
+      paddingBottom: 10,
+     // justifyContent:'space-between'
+
+
+  }
+
+});
