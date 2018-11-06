5de2c719f61a8cddfc8b4e6281953ee1cd0c7056
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-11-06 10:30:45 +0530

    perfected splash screen and login page

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import LoginPage from './src/components/loginpage';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,30 +20,11 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to My App.</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <LoginPage />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
---
+++ src/components/loginpage/index.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import { Container, Header, Content, Form, Item, Input, Label , Button , Body, CheckBox ,ListItem, Text , View } from 'native-base';
+import { StyleSheet , Image } from 'react-native';
+export default class LoginPage extends Component {
+  render() {
+
+              return (
+
+
+              
+                <Container style={styles.container}>
+              
+                <Content>
+                  <Form style={styles.form}>
+                  <Image source={require('../../assets/images/batlogo.png')} style={styles.title} />
+                    <Item fixedLabel>
+                      <Label style={{ color : 'red' }}>Username</Label>
+                      <Input  style={{ color: 'white'}} />
+                    </Item>
+                    <Item fixedLabel last>
+                      <Label style={{ color : 'red' }}>Password</Label>
+                      <Input   style={{ color: 'white'}}  />
+                    </Item>
+                    <Button full bordered light  style={styles.button}>
+                    <Text style={{ color: 'red',textAlign:'center'}}> Sign Up/Sign In </Text>
+                  </Button>
+                  </Form>
+                  <Text style={{ color: 'red',textAlign:'center',marginTop:10}}>Or</Text>
+                  <Text style={{ color: 'white',textAlign:'center',marginTop:10,marginBottom:10}}>Sign in With</Text>
+                  <View style={styles.social}> 
+                        <Button block style={{ marginRight: 10,width:175,height:50 }}><Text> Facebook </Text></Button>
+                        <Button danger style={{ marginRight: 10,width:175,height:50 }} ><Text> Google </Text></Button>         
+                  </View>  
+                  <ListItem>
+                      <CheckBox checked={true} />
+                      <Body>
+                        <Text style={{color : 'white' }}>Discussion with Client</Text>
+                      </Body>
+                  </ListItem>
+                </Content>
+              </Container>
+              )
+  }
+}
+const styles = StyleSheet.create({
+
+
+    form : { 
+
+
+        backgroundColor: 'black',
+        marginTop : 60,
+        
+
+    },
+    container : {
+
+
+
+        backgroundColor : 'black'
+
+    },
+    title: {
+
+      // resizeMode : 'contain',
+      //  paddingTop : 100,
+        alignSelf: 'center',
+        height:100,
+        //width:400
+
+  },
+  button : {
+
+      marginTop : 15,
+      width:400,
+      alignSelf: 'center',
+      backgroundColor : 'black'
+
+
+  },
+  social : {
+
+      flex:  1 ,
+      flexDirection: 'row',
+      padding: 15,
+      alignSelf: 'center',
+      paddingBottom: 10,
+     // justifyContent:'space-between'
+
+
+  }
+
+});
